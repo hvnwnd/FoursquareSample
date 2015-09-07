f0f5990992298f127d8d165cd03cbc1b0e271337
CHEN Bin <chen@MacBookPIC.local>
2015-09-07 14:08:14 +0200

    Request location -> racsignal

--- SNceshi/SNceshi/ViewController.m
+++ SNceshi/SNceshi/ViewController.m
@@ -1,27 +1,86 @@
 //
-//  ViewController.m
-//  SNceshi
+// ViewController.m
+// SNceshi
 //
-//  Created by Titi on 9/1/15.
-//  Copyright (c) 2015 Fantestech. All rights reserved.
+// Created by Titi on 9/1/15.
+// Copyright (c) 2015 Fantestech. All rights reserved.
 //
+
+@import CoreLocation;
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CLLocation *location;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
+    self.locationManager.delegate = self;
+
+    if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+            // We never ask for authorization. Let's request it.
+            [self.locationManager requestWhenInUseAuthorization];
+        } else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse ||
+                   [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways) {
+            // We have authorization. Let's update location.
+            [self.locationManager startUpdatingLocation];
+        } else {
+            // If we are here we have no pormissions.
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"No athorization"
+                                                                message:@"Please, enable access to your location"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"Cancel"
+                                                      otherButtonTitles:@"Open Settings", nil];
+            [alertView show];
+        }
+    }
+
+    RACSignal *locationSignal = RACObserve(self, location);
+
+    [locationSignal subscribeNext:^(id x) {
+        NSLog(@"%@", x);
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+    if (newLocation != nil) {
+        self.location = newLocation;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+    NSLog(@"Location manager did fail with error %@", error);
+    [self.locationManager stopUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [manager startUpdatingLocation];
+    }
 }
 
 @end
