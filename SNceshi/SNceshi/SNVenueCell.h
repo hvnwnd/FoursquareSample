757fdddcc5fa02f8ad01e457057e35fd4696f354
CHEN Bin <chen@MacBookPIC.local>
2015-09-08 14:06:54 +0200

    Rating control

--- SNceshi/SNceshi/SNVenueCell.h
+++ SNceshi/SNceshi/SNVenueCell.h
@@ -6,10 +6,12 @@
 // Copyright (c) 2015 Fantestech. All rights reserved.
 //
 
+#import "EDStarRating.h"
 #import <UIKit/UIKit.h>
 
 @interface SNVenueCell : UITableViewCell
 
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) EDStarRating     *ratingControl;
 
 @end
